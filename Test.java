1f48040181160211d2219073da85822493608d3d
thomas2964 <thomas2964@users.noreply.github.com>
2021-11-14 17:16:50 -0800

    added check2

--- Test.java
+++ Test.java
@@ -4,7 +4,7 @@ interface Test {
    default int sum(int a, int b) { return a + b;}
    static int constant() { return 1;}
    void check(int i);
-   void check2();
+   void check2();  //
    //comment222
    //comment222  nov 15
 }
